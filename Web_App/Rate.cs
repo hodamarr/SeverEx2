a693f24ed4f64731c169680242ac6da6faf745cc
hodamarr <hod110@gmail.com>
2022-05-22 19:28:32 +0300

    first commit

---
+++ Web_App/Rate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Web_App
+{
+	public class Rate
+	{
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public string Content { get; set; }
+    }
+}
+
