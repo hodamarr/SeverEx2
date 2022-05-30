e85d30edd47a9a4119cf530180f39714cf69fdf8
hodamar <hoddavid.amar@live.biu.ac.il>
2022-05-30 18:55:16 +0300

    rate

--- Web_App/Controllers/HomeController.cs
+++ Web_App/Controllers/HomeController.cs
@@ -6,27 +6,61 @@ namespace Web_App.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private static List<Rate> rates = new List<Rate>();
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController()
     {
-        _logger = logger;
+        if (rates.Count == 0)
+        {
+            rates.Add(new Rate { ID = 1, Name = "ofek", Content = "good", Score = "5" });
+            rates.Add(new Rate { ID = 2, Name = "hod", Content = "nice", Score = "3" });
+        }
     }
 
     public IActionResult Index()
     {
-        return View();
+        return View(rates);
     }
 
-    public IActionResult Privacy()
+    [HttpPost]
+    public IActionResult AddReview(string name, string score, string content, string button)
     {
-        return View();
+        int count = rates.Max(i => i.ID) + 1;
+        rates.Add(new Rate()
+        {
+            Content = content,
+            Name = name,
+            Score = score,
+            ID = count
+        });
+
+        return Redirect("Index");
+
     }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Watch(int id)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        Rate r = rates.Find(i => i.ID == id);
+
+        return View(r);
+
+    }
+
+    public IActionResult Edit(int id)
+    {
+        Rate r = rates.Find(i => i.ID == id);
+        return View(r);
+    }
+    /// TODO
+    [HttpPost]
+    public IActionResult Edit(string name, string score, string content, int id)
+    {
+        Rate r = rates.Find(i => i.ID == id);
+        r.Score = score;
+        r.Name = name;
+        r.Content = content;
+
+        return RedirectToAction("Index", r);
     }
 }
 
